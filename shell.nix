a08f1de113a73bce664ef91ccfee82ef655f699f
tabassumhmm <sumaiyatabassum667@gmail.com>
2026-09-25 01:45:59 +0600

    Add OpenSSL to PHP shell

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 pkgs.mkShell {
   packages = [
-    (pkgs.php84.withExtensions ({ all, ... }: with all; [ pdo_sqlite filter mbstring dom tokenizer xmlwriter session ]))
+    (pkgs.php84.withExtensions ({ all, ... }: with all; [ pdo_sqlite filter mbstring dom tokenizer xmlwriter session openssl ]))
     pkgs.php84.packages.composer
     pkgs.sqlite
     pkgs.curl
